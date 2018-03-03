59e732aea571d901215b5165f1e697b0e64f14eb
Arturo Cabellero <acaballero9085@gmail.com>
2018-03-02 21:52:03 -0600

    initial work

--- src/sections/samples/Samples.js
+++ src/sections/samples/Samples.js
@@ -1,4 +1,6 @@
+import ReactPlayer from 'react-player'
 import React, { Component } from 'react'
+import Divider from 'material-ui/Divider'
 
 // Styles
 import './Samples.css'
@@ -14,6 +16,39 @@ class Samples extends Component {
                 <div className='sample-container'>
                     <p className='sample-text'>Sample Work</p>
                 </div>
+
+                <div className='sample-work-container'>
+
+                    <div className='sample-work-area'>
+                        <ReactPlayer className='player' url='https://soundcloud.com/ovosound1/4pm-in-calabasas-explicit' height='200px'/>
+
+                        <ReactPlayer className='player' url='https://www.youtube.com/watch?v=NV-3s2wwC8c'/>
+                    </div>
+
+                </div>
+
+                <Divider/>
+
+                <div className='social-media-links-container'>
+                    <a href="https://www.w3schools.com" target='_blank' rel="noopener noreferrer">
+                        <div className='icon-container'>
+                            <i className="fab fa-facebook-f fa-2x" style={{color: '#fff'}}></i>
+                        </div>
+                    </a>
+
+                    <a href="https://www.w3schools.com" target='_blank' rel="noopener noreferrer">
+                        <div className='icon-container'>
+                            <i className="fab fa-twitter fa-2x" style={{color: '#fff'}}></i>
+                        </div>
+                    </a>
+
+                    <a href="https://www.w3schools.com" target='_blank' rel="noopener noreferrer">
+                        <div className='icon-container'>
+                            <i className="fab fa-instagram fa-2x" style={{color: '#fff'}}></i>
+                        </div>
+                    </a>
+                </div>
+
             </div>
         )
     }
